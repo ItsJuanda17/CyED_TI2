859db48d9d9ac6cb51703d3ad0d4d3c4d15a511b
ItsJuanda17 <1109115477@u.icesi.edu.co>
2023-11-07 16:38:07 -0500

    Save modifications

--- Bomberman/src/main/java/com/example/bomberman/model/Bomb.java
+++ Bomberman/src/main/java/com/example/bomberman/model/Bomb.java
@@ -1,4 +1,76 @@
 package com.example.bomberman.model;
 
-public class Bomb {
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+
+public class Bomb extends GameEntity{
+
+    private int range;
+    private int timer;
+    private boolean exploded;
+    private ArrayList<Image> bombImages;
+    private ArrayList<Image> explosionImages;
+
+    public Bomb(Canvas canvas, int timer, int range) {
+        super(canvas);
+        this.timer = timer;
+        this.range = range;
+        this.exploded = false;
+    }
+
+    public int getTimer() {
+        return this.timer;
+    }
+
+    public int getRange() {
+        return this.range;
+    }
+
+    public boolean isExploded() {
+        return this.exploded;
+    }
+
+    public void setExploded(boolean exploded) {
+        this.exploded = exploded;
+    }
+
+    public void decrementTimer() {
+        this.timer--;
+    }
+
+    public void incrementRange() {
+        this.range++;
+    }
+
+    public void decrementRange() {
+        this.range--;
+    }
+
+    public void explode() {
+        this.exploded = true;
+    }
+
+    public void resetTimer() {
+        this.timer = 3;
+    }
+
+    public void resetRange() {
+        this.range = 1;
+    }
+
+    public void resetExploded() {
+        this.exploded = false;
+    }
+
+    public void reset() {
+        this.resetTimer();
+        this.resetRange();
+        this.resetExploded();
+    }
+
+    @Override
+    public void paint(){
+    }
 }
--- Bomberman/src/main/java/com/example/bomberman/model/Tile.java
+++ Bomberman/src/main/java/com/example/bomberman/model/Tile.java
@@ -1,4 +1,45 @@
 package com.example.bomberman.model;
 
 public class Tile {
+
+    private int x;
+    private int y;
+    private boolean isPassable;
+    private boolean hasBomb;
+
+
+    public Tile(int x, int y, boolean isPassable) {
+        this.x = x;
+        this.y = y;
+        this.isPassable = isPassable;
+        this.hasBomb = false;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isPassable() {
+        return isPassable;
+    }
+
+    public void setPassable(boolean passable) {
+        isPassable = passable;
+    }
+
+    public boolean hasBomb() {
+        return hasBomb;
+    }
+
+    public void placeBomb() {
+        hasBomb = true;
+    }
+
+    public void removeBomb() {
+        hasBomb = false;
+    }
 }
