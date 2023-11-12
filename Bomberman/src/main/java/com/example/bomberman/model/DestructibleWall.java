6054015040a585edd776dd8659f2ef22df832119
Jose Manuel Cardona Rondón <124000426+manxi2004@users.noreply.github.com>
2023-11-12 16:25:53 -0500

    Update DestructibleWall.java

--- Bomberman/src/main/java/com/example/bomberman/model/DestructibleWall.java
+++ Bomberman/src/main/java/com/example/bomberman/model/DestructibleWall.java
@@ -1,4 +1,40 @@
 package com.example.bomberman.model;
 
-public class DestructibleWall {
+import javafx.scene.canvas.Canvas;
+
+import javafx.scene.paint.Color;
+
+public class DestructibleWall extends GameEntity {
+
+    private boolean destroyed;
+    private TileState state;
+
+    public DestructibleWall(Canvas canvas) {
+        super(canvas);
+        this.destroyed = false;
+        this.state = TileState.BREAKABLE_WALL;
+
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void destroy() {
+        this.destroyed = true;
+    }
+
+
+  
+//paint beta
+  
+    @Override
+    public void paint() {
+        gc.setFill(getFillColor());
+        gc.fillRect(position.getPosX() * 10, position.getPosY() * 10, 10, 10);
+    }
+
+    private Color getFillColor() {
+        return state == TileState.BREAKABLE_WALL ? Color.GRAY : Color.BLACK;
+    }
 }
