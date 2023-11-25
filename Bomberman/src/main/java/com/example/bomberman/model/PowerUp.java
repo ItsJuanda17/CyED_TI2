e6e4d5eb9ecf5f49bcf1eb8b9f5005feac6a5fb8
Jose Manuel Cardona Rondón <124000426+manxi2004@users.noreply.github.com>
2023-11-25 01:35:03 -0500

    Update PowerUp.java

--- Bomberman/src/main/java/com/example/bomberman/model/PowerUp.java
+++ Bomberman/src/main/java/com/example/bomberman/model/PowerUp.java
@@ -5,27 +5,33 @@ import javafx.scene.image.Image;
 
 import java.util.Objects;
 
-public class PowerUp extends GameEntity{
+public class PowerUp extends GameEntity {
 
     public enum PowerUpType {
         BOMB_COUNT(new Image(Objects.requireNonNull(PowerUpType.class.getResourceAsStream("/images/powerUps/power_bomb.png")))),
-        BOMB_RANGE(new Image(Objects.requireNonNull(PowerUpType.class.getResourceAsStream("/images/powerUps/power_fireup.png")))),
-        SPEED(new Image(Objects.requireNonNull(PowerUpType.class.getResourceAsStream("/images/powerUps/power_speed.png"))));
+        BOMB_RANGE(new Image(Objects.requireNonNull(PowerUpType.class.getResourceAsStream("/images/powerUps/power_fireup.png"))));
 
-        private Image image;
+        private final Image image;
+
         PowerUpType(Image image) {
+            this.image = image;
         }
+
         public Image getImage() {
             return image;
         }
     }
 
     private PowerUpType type;
+    private Image image;
+    private Map map;
 
-    public PowerUp(Canvas canvas, Vector position, PowerUpType type) {
+    public PowerUp(Canvas canvas, Vector position, PowerUpType type, Map map) {
         super(canvas);
         this.position = position;
         this.type = type;
+        this.map = map;
+        this.image = type.getImage();
     }
 
     public PowerUpType getType() {
@@ -44,22 +50,20 @@ public class PowerUp extends GameEntity{
             case BOMB_RANGE:
                 player.incrementBombRange();
                 break;
-            case SPEED:
-                player.incrementSpeed();
-                break;
         }
     }
 
     @Override
     public void onCollision(GameEntity other) {
-        if(other instanceof Player) {
+        if (other instanceof Player) {
             grantBonus((Player) other);
+            map.getTile(position).setContent(null);
         }
     }
 
     @Override
     public void paint() {
-        gc.drawImage(this.type.getImage(), this.position.getPosX(), this.position.getPosY(), this.width, this.height);
+        gc.drawImage(image, this.position.getPosX()*Tile.TILE_WIDTH, this.position.getPosY()*Tile.TILE_HEIGHT, Tile.TILE_WIDTH-5, Tile.TILE_HEIGHT-5);
     }
 
 }
